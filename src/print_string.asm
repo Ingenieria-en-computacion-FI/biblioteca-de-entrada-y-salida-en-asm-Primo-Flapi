3f704a289b1ade21f32f5b51c3c8c6e85441269b
Aaron <carranzaalexisaaron245@gmail.com>
2026-03-14 20:19:49 -0600

    Add string length calculation and print functionality
    Implemented string length calculation and write syscall.

--- src/print_string.asm
+++ src/print_string.asm
@@ -17,6 +17,33 @@ print_string:
     ; 1. calcular longitud
     ; 2. syscall write
 
+    push esi
+    push ebx
+    push ecx
+    push edx
+
+    mov esi, eax
+    xor ecx, ecx
+
+loop_contador:
+    mov al, [esi+ecx]
+    cmp al, 0  ; ¿Estamos al final de la cadena?
+    je imprimir
+    add ecx, 1
+    jmp loop_contador
+
+imprimir:
+    mov eax, 4
+    mov ebx, 1
+    mov edx, ecx
+    mov ecx, esi
+    int 0x80
+
+    pop edx
+    pop ecx
+    pop ebx
+    pop esi
+
     mov esp, ebp
     pop ebp
     ret
