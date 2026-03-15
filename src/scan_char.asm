75406f5e72356ddafba283961b71b13383cf221c
Aaron <carranzaalexisaaron245@gmail.com>
2026-03-14 20:25:55 -0600

    Implement character reading from stdin
    Added syscall to read a character from stdin into AL.

--- src/scan_char.asm
+++ src/scan_char.asm
@@ -20,6 +20,14 @@ scan_char:
     ; 2. leer 1 byte desde stdin
     ; 3. devolverlo en AL
 
+    mov eax, 3 ; Llamamos a la syscall scan
+    mov ebx, 0 ; STDIN
+    mov ecx, char_buffer ; Apuntamos a la memoria que reserbamos
+    mov edx, 1 ; El tamaño del caracter
+    int 0x80 ; Llamamos al kernel
+
+    mov al, [char_buffer] ; Metemos el byte que leimos a AL
+
     mov esp, ebp
     pop ebp
     ret
