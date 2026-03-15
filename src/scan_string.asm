7abea45cacc95975a91433e57f06aefd0bac2811
Aaron <carranzaalexisaaron245@gmail.com>
2026-03-14 20:26:26 -0600

    Add input reading and null terminator to scan_string
    Implement syscall for reading input and adding null terminator.

--- src/scan_string.asm
+++ src/scan_string.asm
@@ -18,6 +18,20 @@ scan_string:
     ; 2. guardar en buffer
     ; 3. agregar terminador 0
 
+    push eax
+    push ebx
+
+    mov ecx, eax ; Apuntamos al buffer destino
+    mov edx, ebx ; le damos el tamaño maximo
+    mov eax, 3 ; llamamos a la syscall scan
+    mov ebx, 0 ; STDIN
+    int 0x80
+
+    mov ecx, [ebp-4]    ; vamos a la direccion original
+    add ecx, eax        ; vamos al final
+    mov al, 0           ; AL = 0
+    mov [ecx], al       ; escribimos el caracter del final del arreglo
+
     mov esp, ebp
     pop ebp
     ret
