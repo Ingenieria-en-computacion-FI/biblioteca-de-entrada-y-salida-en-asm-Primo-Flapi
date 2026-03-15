e08932ce1bd6bfec66d83b25408214150726039f
Aaron <carranzaalexisaaron245@gmail.com>
2026-03-14 20:17:22 -0600

    Implement byte printing in print_char.asm
    Added assembly code to print a single byte using syscall.

--- src/print_char.asm
+++ src/print_char.asm
@@ -19,6 +19,14 @@ print_char:
 
     ; write(fd=1, buffer, 1)
 
+    push eax
+
+    mov eax, 4 ; Llamamos a la syscall write
+    mov ebx, 1 ; STOUD
+    mov ecx, esp; Apuntamos a la base de la pila
+    mov edx, 1 ; Le decimos el tamaño de caracterres que tenemos que imprimir
+    int 0x80 ; Llamamos al KERNEL
+
     mov esp, ebp
     pop ebp
     ret
